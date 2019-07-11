6e7c5e94f6d3e0f1f58c883b1200f19726e97dce
Fran Valesi <52495018+franvalesi789@users.noreply.github.com>
2019-07-11 12:27:33 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -20,7 +20,14 @@ var mes = document.getElementById('mes').value;
         case "Diciembre": alert("Este mes tiene 30 o más días");
         break;   
     }
-	
+
+    //Otra forma de hacerlo
+
+    /*switch(mes){
+        case "Febrero": alert("Este mes no tiene mas de 29 dias");
+        break;
+        default: alert("Este mes tiene 30 o más días");
+	*/
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -7,18 +7,18 @@ var mes = document.getElementById('mes').value;
         case "Febrero": alert("Este mes tiene 28 dias");
         break;
 
-        case "Abril": 30
-        case "Junio": 30
-        case "Septiembre":30
+        case "Abril": 
+        case "Junio": 
+        case "Septiembre":
         case "Noviembre": alert("Este mes tiene 30 dias");
         break;
 
-        case "Enero": 31
-        case "Marzo":31
-        case "Mayo":31
-        case "Julio":31
-        case "Agosto":31
-        case "Octubre":31
+        case "Enero": 
+        case "Marzo":
+        case "Mayo":
+        case "Julio":
+        case "Agosto":
+        case "Octubre":
         case "Diciembre": alert("Este mes tiene 31 dias");
         break;
     }
