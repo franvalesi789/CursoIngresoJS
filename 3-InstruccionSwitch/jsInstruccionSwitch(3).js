2a57c3823eeb27d49f0b22a4c758078eff6d35de
Fran Valesi <52495018+franvalesi789@users.noreply.github.com>
2019-07-11 11:06:25 -0300

    Add files via upload
    ejercicios switch clase introduccion

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,10 +1,25 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
+var mes = document.getElementById('mes').value;
+
+
+    switch(mes){
+        case "Febrero": alert("Este mes no tiene mas de 29 dias");
+        break;
+        case "Enero":
+        case "Marzo":
+        case "Abril":
+        case "Mayo":
+        case "Junio":
+        case "Julio":
+        case "Agosto":
+        case "Septiembre":
+        case "Octubre":
+        case "Noviembre":
+        case "Diciembre": alert("Este mes tiene 30 o más días");
+        break;   
+    }
 	
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,26 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+var mes = document.getElementById('mes').value;
 
+    switch(mes){
+        case "Febrero": alert("Este mes tiene 28 dias");
+        break;
 
+        case "Abril": 30
+        case "Junio": 30
+        case "Septiembre":30
+        case "Noviembre": alert("Este mes tiene 30 dias");
+        break;
+
+        case "Enero": 31
+        case "Marzo":31
+        case "Mayo":31
+        case "Julio":31
+        case "Agosto":31
+        case "Octubre":31
+        case "Diciembre": alert("Este mes tiene 31 dias");
+        break;
+    }
 
 }//FIN DE LA FUNCIÓN
