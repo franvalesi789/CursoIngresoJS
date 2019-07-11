0d201b47bd93bb90fd39e4af17832af931d6be58
Fran Valesi <52495018+franvalesi789@users.noreply.github.com>
2019-07-11 11:05:17 -0300

    Add files via upload
    ejercicios de if clase introduccion

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,6 +2,18 @@ function mostrar()
 {
 //tomo la edad  
 
+    var edad;
+    var estado;
+
+    edad = parseInt(document.getElementById("edad").value);
+
+    estado = parseInt(document.getElementById("estadoCivil").value);
+
+    if (edad > 18 && estado == "soltero"){
+        alert("Es soltero y no es menor");
+    }
+
+    
 	
 
 
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,11 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var maximo = 10;
+	var minimo = 1;
+
+	var numRandom = Math.round(Math.random() * (maximo - minimo));
+
+	alert(numRandom);
 
 }//FIN DE LA FUNCIÓN
